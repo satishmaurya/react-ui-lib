074c85c097aa603809f817395545ac607b1d5f61
satish <satish@Thinks-MacBook-Pro.local>
2020-03-19 18:13:48 +0530

    add variable for button component

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -4,18 +4,46 @@ import * as variable from "../../variable";
 const StyledButton = styled.button`
     padding: 6px 16px;
     border-radius: 4px;
+    color: ${variable.white};
     ${props =>
         props.color === "primary"
         ? css `
-            background: ${variable.ColorBlue};
-            color: ${variable.ColorWhiteBase};
+            background: ${variable.blue};
         `
         : props.color === "secondary"
            ? css `
-            background: ${variable.ColorRed};
-           `
+            background: ${variable.gray600};
+        `
+        : props.color === "success"
+        ? css `
+         background: ${variable.green};
+        `
+        : props.color === "info"
+        ? css `
+         background: ${variable.cyan};
+        `
+        : props.color === "warning"
+        ? css `
+         background: ${variable.yellow};
+        `
+        : props.color === "danger"
+        ? css `
+         background: ${variable.orange};
+        `
+        : props.color === "purple"
+        ? css `
+         background: ${variable.purple};
+        `
+        : props.color === "light"
+        ? css `
+         background: ${variable.gray100};
+        `
+        : props.color === "dark"
+        ? css `
+         background: ${variable.gray800};
+        `
            : css`
-                background: ${variable.ColorGrey}
+                background: ${variable.gray800}
            `
     }
     ${props =>
