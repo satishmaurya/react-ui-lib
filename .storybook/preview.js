e3d22b95666549f8b6f97ae92d793acad2b7b85a
satish <satish@Thinks-MacBook-Pro.local>
2020-04-20 16:51:55 +0530

    inhance storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -16,8 +16,9 @@ const theme = create({
     base: 'light',
     brandTitle: 'Stack UI',
     brandUrl: 'https://successive.tech',
-    brandImage:
-      'logoShap.png',
+    brandTitle: `<b style="color:#3c4edd"><i style="font-size:30px;">S</i>tack UI</b><br /><small><span style="font-weight: 300">Component Library from</span> Successive Tech</small><br /><small>v.0.1.0</small>`,
+    // brandImage:
+    //   'logoShap.png',
   });
 
   addParameters({
