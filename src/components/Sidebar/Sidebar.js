284ad478206c72c3dc8122fdad6c487a9ac2e401
shubham2607 <shubham.mishra@successive.tech>
2020-04-21 17:27:17 +0530

    feature: Sidebar component added as testing

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {StyledSideBar, MainWrapper} from './Sidebar.styled';
+import Button from '../Button/Button';
+
+const Sidebar = (props) => {
+  const { children, id, isDrawer} = props;
+
+  const handleOpen = () => {
+    document.getElementById("Sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  const handleClose = () => {
+    document.getElementById("Sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  if(isDrawer===true){
+    return(
+        <>
+        <StyledSideBar id={id} isDrawer>
+            <a className="closeIcon" onClick={handleClose}>&times;</a>
+            {children}
+        </StyledSideBar>
+        <MainWrapper id="main">
+            <h2>SideBar</h2>
+            <Button onClick={handleOpen} btnType="success">Open</Button>
+        </MainWrapper>
+    </>
+    )}
+    else {
+        return(
+            <>
+            <StyledSideBar>
+                {children}
+            </StyledSideBar>
+        </>
+        )
+    }
+}
+
+export default Sidebar;
