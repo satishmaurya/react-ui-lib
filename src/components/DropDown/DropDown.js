7b10ea7857a1db4ca98c0dae272b821a475c0c24
kajol1994 <kajol.singh@successive.tech>
2020-04-21 21:03:59 +0530

    work on the dropdown component

--- src/components/DropDown/DropDown.js
+++ src/components/DropDown/DropDown.js
@@ -3,19 +3,24 @@ import StyledDropDown from "./DropDown.styled";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
-const DropDown = (props) =>{
-    const {fixed, isDisabled, btnVariant, rounded, children, size, block, icon,iconVariant} = props;
-    return(
-        <StyledDropDown size={size} {...props}>
-           <StyledDropDown.Toggle variant="primary" id="dropdown-basic" size="sm">
-            Dropdown Button
+const DropDown = (props) => {
+const { btnType, id, isDisabled, className, btnVariant,children, rounded,  size, block, Iconsize, icon, iconVariant, options,  } = props;
+return (
+    
+        <StyledDropDown>
+            <StyledDropDown.Toggle variant= {btnType} className={className} type={btnVariant} size={size} disabled={isDisabled} rounded={rounded} block={block}  id= {id} {...props}>
+               DropDown
+               {icon && iconVariant==='regular' ? <FontAwesomeIcon icon={['far',icon]} Iconsize={Iconsize}  /> : iconVariant==='solid' ? <FontAwesomeIcon icon={['fas',icon]}  Iconsize={Iconsize} /> : <FontAwesomeIcon icon={['fas',icon]} Iconsize={Iconsize}/> }
+                {children}
             </StyledDropDown.Toggle>
             <StyledDropDown.Menu>
-                <StyledDropDown.Item href="#/action-1">Action</StyledDropDown.Item>
-                <StyledDropDown.Item href="#/action-2">Another action</StyledDropDown.Item>
-                <StyledDropDown.Item href="#/action-3">Something else</StyledDropDown.Item>
+                {options.map((item, key) => (
+                    <StyledDropDown.Item  href={item.link}>{item.item}</StyledDropDown.Item>
+                ))}
+                
             </StyledDropDown.Menu>
         </StyledDropDown>
+                
     );
 }
 
--- src/components/DropDown/DropDown.types.js
+++ src/components/DropDown/DropDown.types.js
@@ -4,12 +4,17 @@ import PropTypes from "prop-types";
 const DropDownTypes= () => <>DropDownTypes</>;
 
 DropDownTypes.propTypes={
+    
+    id:PropTypes.string,
+    className:PropTypes.string,
+    options:PropTypes.arrayOf(PropTypes.shape({ }) ),
     btnType: PropTypes.oneOf(['primary','secondary','success','info','warning','danger','purple','light','dark']),
     btnVariant: PropTypes.oneOf(['contained', 'outlined']),
     isDisabled: PropTypes.bool,
     rounded:PropTypes.oneOf(['rounded','default']),
     size:PropTypes.oneOf(['sm','md','lg']),
-    block:PropTypes.bool
+    block:PropTypes.bool,
+    Iconsize:PropTypes.oneOf(['sm', 'md', 'lg']),
 };
 
 DropDownTypes.defaultProps = {
@@ -18,7 +23,11 @@ DropDownTypes.defaultProps = {
     isDisabled: false,
     rounded:'default',
     size:'md',
-    block:false
+    block:false,
+    Iconsize:'md',
+    id:'string',
+    className:'string',
+    options:'ArrayOf(object)'
 };
 
 export default DropDownTypes;
