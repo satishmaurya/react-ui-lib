df4cea1a98ca5e9b7d0584ec0dfa5fa12528a511
shubham2607 <shubham.mishra@successive.tech>
2020-04-22 19:32:33 +0530

    Avatar component added

---
+++ src/components/Avatar/Avatar.types.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const AvatarTypes= () => <>AvatarTypes</>;
+
+AvatarTypes.propTypes={
+  src: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(['sm','md', 'lg']),
+  alt: PropTypes.string,
+};
+
+AvatarTypes.defaultProps = {};
+
+export default AvatarTypes;
