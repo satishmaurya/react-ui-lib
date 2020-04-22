77ff9ffa6dc115beb777150ee51e03776cd7ca0d
satish <satish@Thinks-MacBook-Pro.local>
2020-04-22 20:03:05 +0530

    add accordion

--- src/components/Accordian/Accordian.js
+++ src/components/Accordian/Accordian.js
@@ -3,14 +3,14 @@ import StyledAccordion from "./Accordian.styled";
 import Card from 'react-bootstrap/Card';
 
 const Accordian = (props) =>{
-    const{ color, bgColor, children, cardTitle, width, alignText, alignCard, imgUrl, subTitle, imgPos, border} =props;
+    const{ color, bgColor, children, cardTitle, width, headerText, alignCard, imgUrl, subTitle, imgPos, border} =props;
     return(
         <StyledAccordion imgUrl={imgUrl} color={color} bgColor={bgColor} {...props}>
-             <StyledAccordion.Toggle variant="link" eventKey="0">
-        Click me!
+             <StyledAccordion.Toggle as={Card.Header} variant="link" eventKey="0">
+                {headerText}
       </StyledAccordion.Toggle>
       <StyledAccordion.Collapse eventKey="0">
-      <div>Hello! I'm the body</div>
+      {children}
     </StyledAccordion.Collapse>
         </StyledAccordion>
     )
