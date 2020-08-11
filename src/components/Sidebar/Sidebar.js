d6f069779e31de37dfa527daf2150a2c00d20e4a
sriya_pathak <sriya.pathak@successive.tech>
2020-08-11 12:49:26 +0530

    resolved sidebar issue

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,12 @@
 import React from "react";
-import {StyledSideBar, MainWrapper} from './Sidebar.styled';
+import {StyledSideBar, MainWrapper, StyledSideBarBody, StyledSideBarTitle} from './Sidebar.styled';
+// import { Button } from '../index';
 import Button from '../Button/Button';
+// import Typography from '../Typography/Typography';
 
 const Sidebar = (props) => {
-  const { children, id, isDrawer, position, onClose, isCloseButtonShown} = props;
+  const { children, id, isDrawer, title,titleBg, position, onClose, isCloseButtonShown} = props;
+console.log('propsss------', titleBg);
 
   const handleOpen = () => {
     document.getElementById("Sidebar").style.width = "250px";
@@ -18,21 +21,44 @@ const Sidebar = (props) => {
   if(isDrawer===true){
     return(
         <>
-        <StyledSideBar id={id} isDrawer {...props}>
+        {title ?  <>
+        <StyledSideBar id={id} isDrawer {...props}  titleBg={titleBg}>
+            <a className="closeIcon" onClick={handleClose}>&times;</a>
+            <StyledSideBarTitle>
+              {title}
+            </StyledSideBarTitle>
+          <StyledSideBarBody>
+          {children}
+          </StyledSideBarBody>
+        </StyledSideBar>
+        <MainWrapper id="main">
+            {/* <h2>SideBar</h2> */}
+            <Button onClick={handleOpen} btnType="success">Open</Button>
+        </MainWrapper>
+    </> :
+        <>
+           <StyledSideBar id={id} isDrawer>
             <a className="closeIcon" onClick={handleClose}>&times;</a>
             {children}
         </StyledSideBar>
         <MainWrapper id="main">
-            <h2>SideBar</h2>
+            {/* <h2>SideBar</h2> */}
             <Button onClick={handleOpen} btnType="success">Open</Button>
         </MainWrapper>
+        </>
+        }
     </>
     )}
     else {
         return(
             <>
-            <StyledSideBar {...props}>
-                {children}
+            <StyledSideBar {...props}  titleBg={titleBg} title>
+            <StyledSideBarTitle>
+              {title}
+            </StyledSideBarTitle>
+          <StyledSideBarBody>
+          {children}
+          </StyledSideBarBody>
             </StyledSideBar>
         </>
         )
