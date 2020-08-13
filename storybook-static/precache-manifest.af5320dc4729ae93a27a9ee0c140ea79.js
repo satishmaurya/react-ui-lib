63246a454bff5d637536744ac4254f808f20a244
satishmaurya <satish.maurya@dentsuaegis.com>
2020-08-13 11:26:25 +0530

    build

---
+++ storybook-static/precache-manifest.af5320dc4729ae93a27a9ee0c140ea79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e9cb339c9e826814ac0be364206bb8",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.b77cf8e76e46ab70ee1b.bundle.js"
+  },
+  {
+    "url": "runtime~main.b77cf8e76e46ab70ee1b.bundle.js"
+  },
+  {
+    "revision": "3671ec39639af12602b1",
+    "url": "static/css/main.f4e0026c.chunk.css"
+  },
+  {
+    "revision": "1f9c0cf0824c7d54654e",
+    "url": "static/css/vendors~main.11829350.chunk.css"
+  },
+  {
+    "url": "vendors~main.b77cf8e76e46ab70ee1b.bundle.js"
+  },
+  {
+    "url": "vendors~main.b77cf8e76e46ab70ee1b.bundle.js.LICENSE.txt"
+  }
+]);
