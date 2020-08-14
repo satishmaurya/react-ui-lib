76a77a556c78c59b800e25df4c71b40403d45652
shubham2607 <shubham.mishra@successive.tech>
2020-08-15 02:15:34 +0530

    simple sidebar added

--- dist/esm/components/Sidebar/Sidebar.styled.js
+++ dist/esm/components/Sidebar/Sidebar.styled.js
@@ -5,7 +5,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.MainWrapper = exports.StyledSideBar = exports.StyledSideBarTitle = exports.StyledSideBarBody = exports.StyledSideBarWrap = void 0;
+exports.StyledSideBar = void 0;
 
 var _styledComponents = _interopRequireWildcard(require("styled-components"));
 
@@ -13,43 +13,11 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-var StyledSideBarWrap = _styledComponents["default"].div.withConfig({
-  displayName: "Sidebarstyled__StyledSideBarWrap",
-  componentId: "sc-3ocxlf-0"
-})(["padding-top:15px;"]);
-
-exports.StyledSideBarWrap = StyledSideBarWrap;
-
-var StyledSideBarBody = _styledComponents["default"].div.withConfig({
-  displayName: "Sidebarstyled__StyledSideBarBody",
-  componentId: "sc-3ocxlf-1"
-})([""]);
-
-exports.StyledSideBarBody = StyledSideBarBody;
-
-var StyledSideBarTitle = _styledComponents["default"].div.withConfig({
-  displayName: "Sidebarstyled__StyledSideBarTitle",
-  componentId: "sc-3ocxlf-2"
-})(["", ","], function (props) {
-  return props.title ? (0, _styledComponents.css)(["border:1px solid pink;"]) : (0, _styledComponents.css)(["background:#03A9F4;color:#ffffff;font-weight:600;padding:20px;"]);
-});
-
-exports.StyledSideBarTitle = StyledSideBarTitle;
-
 var StyledSideBar = _styledComponents["default"].div.withConfig({
   displayName: "Sidebarstyled__StyledSideBar",
-  componentId: "sc-3ocxlf-3"
-})(["width:250px;height:auto;position:fixed;z-index:1;top:0;left:auto;", " ", ""], function (props) {
-  return props.position === 'right' ? (0, _styledComponents.css)(["left:auto;right:0;"]) : props.position === 'left' ? (0, _styledComponents.css)(["left:0;"]) : (0, _styledComponents.css)([""]);
-}, function (props) {
-  return props.bgColor === 'dark' ? (0, _styledComponents.css)(["background:#3d454e;color:#fff;"]) : props.bgColor === 'light' ? (0, _styledComponents.css)(["background:#fff;color:#322f2f;"]) : (0, _styledComponents.css)([""]);
+  componentId: "sc-3ocxlf-0"
+})(["position:fixed;top:0px;left:0px;z-index:101;height:100vh;width:250px;padding:10px;overflow:auto;", ""], function (props) {
+  return props.background ? (0, _styledComponents.css)([" background:", ";"], props.background) : (0, _styledComponents.css)([""]);
 });
 
 exports.StyledSideBar = StyledSideBar;
-
-var MainWrapper = _styledComponents["default"].div.withConfig({
-  displayName: "Sidebarstyled__MainWrapper",
-  componentId: "sc-3ocxlf-4"
-})(["transition:margin-left .5s;padding:16px;height:600px;"]);
-
-exports.MainWrapper = MainWrapper;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,80 +1,14 @@
 /* eslint-disable no-unused-expressions */
 import React from "react";
-import {StyledSideBar, MainWrapper, StyledSideBarBody, StyledSideBarTitle} from './Sidebar.styled';
-import Button from '../Button/Button';
+import { StyledSideBar } from './Sidebar.styled';
 
 const Sidebar = (props) => {
-  const { children, id, isDrawer, title,titleBg, bgColor, position, onClose, isCloseButtonShown} = props;
-  // {{'backgroundColor': status === 'approved' ? 'blue' : status === 'pending' ? 'black' : 'red'}}
-  const handleOpen = () => {
-    document.getElementById("Sidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  const handleClose = () => {
-    document.getElementById("Sidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
-  
-  return(
-    <StyledSideBar id={id} title={title} position={position} {...props}>
-      {
-        title ? 
-        (
-            <StyledSideBar>
-            <StyledSideBarTitle>
-              {title}
-            </StyledSideBarTitle>
-          <StyledSideBarBody>
-          {children}
-          </StyledSideBarBody>
-           </StyledSideBar>
-        ) 
-        :
-        isDrawer ==='true' 
-        ? (
-          <>
-          <StyledSideBar id={id} isDrawer>
-           <a className="closeIcon" onClick={handleClose}>&times;</a>
-           {children}
-       </StyledSideBar>
-       <MainWrapper id="main">
-           <Button onClick={handleOpen} btnType="success">Open</Button>
-       </MainWrapper>
-       </>
-        ) 
-        :  
-        <StyledSideBar {...props}>
-        {children}
-    </StyledSideBar>
-      }
+  const { children } = props;
+  return (
+    <StyledSideBar {...props}>
+      {children}
     </StyledSideBar>
   )
-  
-    }
-/* ============As for now drawer functionality is on hold==============*/
-
-/*const handleOpen = () => {
-  document.getElementById("Sidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
 }
-
-const handleClose = () => {
-  document.getElementById("Sidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
-  if(isDrawer===true){
-    return(
-        <>
-           <StyledSideBar id={id} isDrawer>
-            <a className="closeIcon" onClick={handleClose}>&times;</a>
-            {children}
-        </StyledSideBar>
-        <MainWrapper id="main">
-            <Button onClick={handleOpen} btnType="success">Open</Button>
-        </MainWrapper>
-        </>
-    )}   */
-
 
 export default Sidebar;
