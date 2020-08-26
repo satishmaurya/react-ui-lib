a3eb9b6828e43f69a0de1e861c6841676e944a88
sriya_pathak <sriya.pathak@successive.tech>
2020-08-26 17:11:56 +0530

    updated props in the components

--- src/components/CheckboxGroup/Checkbox.js
+++ src/components/CheckboxGroup/Checkbox.js
@@ -5,8 +5,6 @@ const Checkbox = (props) => {
   const { label, disabled, error, onClick, id, name, value  } = props;
   return (
     <StyledCheckbox {...props}>
-         {/* <input type="checkbox" id={id} name={name} value={value} />
-  <label for="vehicle1"> {label} </label>  */}
     <label class="checklabel">{label} 
       <input type="checkbox"  id={id} name={name} value={value} disabled={disabled} {...props} />
       <span class="checkmark"></span>
