48bfdf1bb7e14ed43d6f9334f2de0e52823ecff0
sriya_pathak <sriya.pathak@successive.tech>
2020-08-31 20:04:13 +0530

    textinput

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { StyledTextInput } from './TextInput.styled';
-import InputGroup from 'react-bootstrap/InputGroup'
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
+
 // import
 
 const TextInput = (props) => {
-  const { label, disabled, error, onClick, hint, placeholder, value, name  } = props;
+  const { label, disabled, error, onClick, hint, placeholder, value, name, size,
+  bsPrefix, rows, as  } = props;
   return (
     <StyledTextInput {...props}>
+       <Form.Label>{label}</Form.Label> 
+                      <Form.Control as={as} rows={rows} bsPrefix={bsPrefix} size={size} {...props} /> 
+  
       {/* <label>{label}
   <input class="form-control" type="text" name={name} value={value}
     disabled={disabled} error={error} {...props} /> */}
