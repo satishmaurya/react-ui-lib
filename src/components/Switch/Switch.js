1b0544638bec39511e064e6366e2eb7de2065909
sriya_pathak <sriya.pathak@successive.tech>
2020-09-14 10:25:24 +0530

    fixed switched props issue

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import { StyledSwitch, SwitchLabel } from './Switch.styled';
 
-const Switch = (props) => {
-const { onClick, size, id, name, label, isBold, color, isChecked } = props;
-        return (
+class Switch extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      check: props.isChecked ? true : false,
+    };
+  }
+  handleChecked = (e) => {
+    const { check } = this.state;
+    this.setState({
+      check: !check,
+    })
+  }
+
+  render() {
+  const { onClick, size, id, name, label, isBold, color, isChecked } = this.props;
+         return (
             <StyledSwitch>
                 { label && 
                 <SwitchLabel isBold={isBold} color={color}>
                     {label}
                 </SwitchLabel>
                  }
-                <label class="switch">
-                 { isChecked ? 
-                 ( <>
-                    <input type="checkbox" id={id} name={name}
-                    onClick={onClick} size={size} checked />
-                    {/* <span class="slider round"></span> */}
-                    </>
-                  )
-                  : ( <>
-                    <input type="checkbox" id={id} name={name}
-                    onClick={onClick} size={size}  />
-                    </>
-                  )
-                }
+                <label class="switch" >
+                <input  type="checkbox" id={id} name={name} checked={this.state.check}  onClick={this.handleChecked} />
                 <span class="slider round"></span>
                 </label>
             </StyledSwitch>
         );
     }
-
+  };
 
 export default Switch;
