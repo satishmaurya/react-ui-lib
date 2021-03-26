5e82cfa7294b07283cdccaea0e341d47ac580f8b
Sriya Pathak <sriya.pathak@sriya298ub.successivetech.com>
2021-03-26 14:01:28 +0530

    fixed checkbox & stacktabs issue~

---
+++ src/components/StackTabs/index.js
@@ -0,0 +1,1 @@
+export { default as StackTabs } from './StackTabs';
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ export { Sidebar } from './components/Sidebar';
 export { Spinner } from './components/Spinner';
 export { StackChart } from './components/StackChart';
 export { Table } from './components/Table';
-export { Tabs } from './components/Tabs';
+export { StackTabs } from './components/StackTabs';
 export { Pagination } from './components/Pagination';
 export { RadialProgressBar } from './components/RadialProgressBar';
 export { Typography } from './components/Typography';
